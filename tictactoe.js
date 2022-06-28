f2d0d22504eb57c8f5c42d399a7242bbb40625ed
Marwen <106754847+MiroJlassi@users.noreply.github.com>
2022-06-28 05:57:19 +0100

    v0.0.1

---
+++ tictactoe.js
@@ -0,0 +1,158 @@
+
+i = 0;
+
+
+
+
+// 1
+function lab_1()
+{
+  if (document.getElementById('lab1').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab1').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab1').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 2
+function lab_2()
+{
+  if (document.getElementById('lab2').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab2').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab2').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 3
+function lab_3()
+{
+  if (document.getElementById('lab3').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab3').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab3').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 4
+function lab_4()
+{
+  if (document.getElementById('lab4').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab4').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab4').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 5
+function lab_5()
+{
+  if (document.getElementById('lab5').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab5').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab5').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 6
+function lab_6()
+{
+  if (document.getElementById('lab6').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab6').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab6').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 7
+function lab_7()
+{
+  if (document.getElementById('lab7').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab7').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab7').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 8
+function lab_8()
+{
+  if (document.getElementById('lab8').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab8').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab8').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
+
+// 9
+function lab_9()
+{
+  if (document.getElementById('lab9').innerHTML == "")
+  {
+    if (i % 2 == 0)
+    {
+      document.getElementById('lab9').innerHTML = "X";
+      i+=1;
+    }
+    else {
+      document.getElementById('lab9').innerHTML = "O";
+      i+=1;
+    }
+  }
+}
